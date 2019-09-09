46ac0631e563b8bea67ab4d7b6cc882d4a24d7b5
Kshitiz Sharma <kshitiz.sharma@publicissapient.com>
2019-09-09 19:40:08 +0530

    refactored code

---
+++ src/main/java/org/cloud/proxy/utils/DefaultProxy.java
@@ -0,0 +1,25 @@
+package org.cloud.proxy.utils;
+
+import org.cloud.proxy.CloudBaseRemoteProxy;
+
+public class DefaultProxy extends AbstractCloudProxy {
+
+	public void addANode() {
+		// TODO Auto-generated method stub
+	}
+
+	public void removeANode() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void updateProxyNodes(CloudBaseRemoteProxy pCloudBaseRemoteProxy) {
+		if (super.requireToAddANode(pCloudBaseRemoteProxy)) {
+			addANode();
+		} else if (super.requireToRemoveANode(pCloudBaseRemoteProxy)) {
+			removeANode();
+		}
+
+	}
+
+}
