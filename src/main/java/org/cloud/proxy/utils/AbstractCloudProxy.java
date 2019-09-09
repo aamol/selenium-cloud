4596e2fb414383e136fcc86f9fe6c4add7c7c4ef
Kshitiz Sharma <kshitiz.sharma@publicissapient.com>
2019-09-09 20:38:22 +0530

    Refactored code

--- src/main/java/org/cloud/proxy/utils/AbstractCloudProxy.java
+++ src/main/java/org/cloud/proxy/utils/AbstractCloudProxy.java
@@ -3,12 +3,13 @@
  */
 package org.cloud.proxy.utils;
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.cloud.proxy.CloudBaseRemoteProxy;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.openqa.grid.internal.ProxySet;
 import org.openqa.grid.internal.RemoteProxy;
 import org.openqa.grid.internal.TestSession;
@@ -18,52 +19,84 @@ import org.openqa.grid.internal.TestSession;
  *
  */
 public abstract class AbstractCloudProxy implements CloudProxy {
+	
+	private List<String> removeNodes;
+	private Map<String, Integer> activeNodes;
+
+	/**
+	 * @return the removeNodes
+	 */
+	public List<String> getRemoveNodes() {
+		return removeNodes;
+	}
+
+	/**
+	 * @param removeNodes the removeNodes to set
+	 */
+	public void setRemoveNodes(List<String> removeNodes) {
+		this.removeNodes = removeNodes;
+	}
+
+	/**
+	 * @return the activeNodes
+	 */
+	public Map<String, Integer> getActiveNodes() {
+		return activeNodes;
+	}
+
+	/**
+	 * @param activeNodes the activeNodes to set
+	 */
+	public void setActiveNodes(Map<String, Integer> activeNodes) {
+		this.activeNodes = activeNodes;
+	}
 
 	/**
 	 * method will detect busy and idle nodes associated with a specified cloud type
 	 */
-	public JSONObject getAllRegisteredNodes(CloudBaseRemoteProxy pCloudBaseRemoteProxy) {
-		JSONObject allNodes = new JSONObject();
+	public void getAllRegisteredNodes(CloudBaseRemoteProxy pCloudBaseRemoteProxy) {
 
 		if (null != pCloudBaseRemoteProxy) {
+			
 			ProxySet proxies = pCloudBaseRemoteProxy.getRegistry().getAllProxies();
+			
 			if (null != proxies) {
+			
 				Iterator<RemoteProxy> iterator = proxies.iterator();
-				JSONArray busyProxies = new JSONArray();
-				JSONArray freeProxies = new JSONArray();
+				
 				while (iterator.hasNext()) {
 					RemoteProxy eachProxy = iterator.next();
 					
-					Set<TestSession> activeSessions = eachProxy.getRegistry().getActiveSessions();
-					System.out.println("***** Active Sessions :  *****"+ activeSessions.size());
-					
-					if (eachProxy.isBusy()) {
-						busyProxies.put(eachProxy.getOriginalRegistrationRequest().toJson());
-					} else {
-						freeProxies.put(eachProxy.getOriginalRegistrationRequest().toJson());
+					if(null != eachProxy) {
+						Set<TestSession> activeSessions = eachProxy.getRegistry().getActiveSessions();
+						System.out.println("***** Active Sessions :  *****"+ activeSessions.size());
+						
+						//to get all used proxies that are in use
+						//eachProxy.getRegistry().getUsedProxies()
+						
+						if(null != null && activeSessions.size() == 0) {
+							getRemoveNodes().add(eachProxy.getId());
+						} else {
+							getActiveNodes().put(eachProxy.getId(), activeSessions.size());
+						}
+						
+						//eachProxy.getMaxNumberOfConcurrentTestSessions()
+						}	
 					}
-				}
-
-				allNodes.put("BusyProxies", busyProxies);
-				allNodes.put("FreeProxies", freeProxies);
-			}
-		}
+				}//end of while
+			}//end of if
 		
-		return allNodes;
 	}// end of method
 
 	/**
 	 * method to check if need to add a node to the proxy
 	 */
 	public boolean requireToAddANode(CloudBaseRemoteProxy pCloudBaseRemoteProxy) {
-		JSONObject allNodes = getAllRegisteredNodes(pCloudBaseRemoteProxy);
+		getAllRegisteredNodes(pCloudBaseRemoteProxy);
 
-		if (null != allNodes) {
-			JSONArray busyProxies = allNodes.getJSONArray("BusyProxies");
-
-			if (null != busyProxies && busyProxies.length() == 5) {
-				return true;
-			}
+		if (null != this.getActiveNodes() && this.getActiveNodes().size() > 0) {
+			//TO-DO: Check if configured nodes & max active sessions got exausted
+			return true;
 		}
 
 		return false;
@@ -73,15 +106,12 @@ public abstract class AbstractCloudProxy implements CloudProxy {
 	 * method to check if need to remove a node in case if node is ideal and not executing any test cases
 	 */
 	public boolean requireToRemoveANode(CloudBaseRemoteProxy pCloudBaseRemoteProxy) {
-		JSONObject allNodes = getAllRegisteredNodes(pCloudBaseRemoteProxy);
+		getAllRegisteredNodes(pCloudBaseRemoteProxy);
 
-		if (null != allNodes) {
-			JSONArray freeProxies = allNodes.getJSONArray("FreeProxies");
-
-			if (null != freeProxies && freeProxies.length() == 0) {
-				return true;
-			}
+		if (null != this.getRemoveNodes() && this.getRemoveNodes().size() > 0) {
+			return true;
 		}
+		
 		return false;
 	}
 
